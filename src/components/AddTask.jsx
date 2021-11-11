a0d6f612494070d8340415326ae4edd4646cc583
Elena <rhapsodyenmauve@gmail.com>
2021-11-11 01:07:59 +0000

    basic add task form styling css

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,20 +4,20 @@ import './AddTask.styles.css';
 const AddTask = () => {
   return (
     <form className='add-form'>
-      <div>
+      <div className='input-field'>
         <label htmlFor='add-task'>Task</label>
         <input type='text' placeholder='Add Task' id='add-task' />
       </div>
-      <div>
+      <div className='input-field'>
         <label htmlFor='day-time'>Day & Time</label>
         <input type='text' placeholder='Add Day & Time' id='day-time' />
       </div>
-      <div>
+      <div className='input-check'>
         <label htmlFor='reminder'>Set Reminder</label>
         <input type='checkbox' id='reminder' />
       </div>
 
-      <input type='submit' value='Save Task' />
+      <input type='submit' value='Save Task' className='btn-add'/>
     </form>
   );
 };
