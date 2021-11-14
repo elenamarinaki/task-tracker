9821137545c9e66161284515cba2ab177b7199b9
Elena <rhapsodyenmauve@gmail.com>
2021-11-14 00:15:33 +0000

    setting state for each input field

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import './AddTask.styles.css';
 
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState('');
+
   return (
     <form className='add-form'>
       <div className='input-field'>
         <label htmlFor='add-task'>Task</label>
-        <input type='text' placeholder='Add Task' id='add-task' />
+        <input
+          type='text'
+          placeholder='Add Task'
+          id='add-task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className='input-field'>
         <label htmlFor='day-time'>Day & Time</label>
-        <input type='text' placeholder='Add Day & Time' id='day-time' />
+        <input
+          type='text'
+          placeholder='Add Day & Time'
+          id='day-time'
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className='input-check'>
         <label htmlFor='reminder'>Set Reminder</label>
-        <input type='checkbox' id='reminder' />
+        <input
+          type='checkbox'
+          id='reminder'
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
 
-      <input type='submit' value='Save Task' className='btn-add'/>
+      <input type='submit' value='Save Task' className='btn-add' />
     </form>
   );
 };
